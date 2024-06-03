2d0e910e5eeff53327cff094685f50afca39daae
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-03 22:53:05 +0500

    Improves the appearance of experience section

--- src/components/custom/ExperienceSection.tsx
+++ src/components/custom/ExperienceSection.tsx
@@ -18,7 +18,7 @@ export function ExperienceSection({
 }: {
   readonly data: ExperienceSectionProps;
 }) {
-  const { title, sectionId } = data;
+  const { title, sectionId, workExperience } = data;
 
   return (
     <Wrapper
@@ -26,8 +26,45 @@ export function ExperienceSection({
       id={sectionId}
       className="mb-8">
       <TheHeading level="2">{title}</TheHeading>
+
+      <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-trangray-x-px md:before:ml-[8.75rem] md:before:trangray-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-gray-300 before:to-transparent">
+        {workExperience.map((experience) => (
+          <div
+            key={experience.id}
+            className="relative">
+            <div className="md:flex items-center md:space-x-4 mb-3">
+              <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                {/* Icon */}
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                  <svg
+                    className="fill-emerald-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={16}
+                    height={16}>
+                    <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
+                  </svg>
+                </div>
+                {/* Date */}
+                <time className="font-caveat font-medium text-xl text-primary md:w-28">
+                  {experience.period}
+                </time>
+              </div>
+              {/* Title */}
+              <div className="ml-14 flex flex-col lg:flex-row gap-2 items-baseline">
+                <TheHeading level="3">{experience.company}</TheHeading>
+                <BodyText>{experience.position}</BodyText>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="bg-white p-4 rounded border border-gray-200 text-gray-500 shadow ml-14 md:ml-44">
+              {experience.description}
+            </div>
+          </div>
+        ))}
+      </div>
+
       <div className="flex flex-col gap-4 md:gap-6">
-        {data.workExperience.map((experience) => (
+        {/* {workExperience.map((experience) => (
           <div
             key={experience.id}
             className="flex flex-col lg:flex-row gap-4 mb-3 md:mb-6">
@@ -45,7 +82,7 @@ export function ExperienceSection({
               </div>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </Wrapper>
   );
